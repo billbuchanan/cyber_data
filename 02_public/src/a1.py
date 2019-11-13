138f4f106680f68ef13cb5284a3e7c4c5a8b366c
Bill Buchanan <w.buchanan@napier.ac.uk>
2019-11-13 15:46:42 +0000

    Update a1.py

--- 02_public/src/a1.py
+++ 02_public/src/a1.py
@@ -1,3 +1,4 @@
+# Python 2.7
 p=11
 q=3
 N=p*q
@@ -5,11 +6,27 @@ PHI=(p-1)*(q-1)
 e=3
 for d in range(1,100):
         if ((e*d % PHI)==1): break
-print e,N
-print d,N
+print (e,N)
+print (d,N)
 M=4
 cipher = M**e % N
-print cipher
+print ("Cipher is: ",cipher)
 message = cipher**d % N
-print message
+print (message)
+
+# Python 3  - just uncomment if you are using Python 3
+# p=11
+#q=3
+#N=p*q
+#PHI=(p-1)*(q-1)
+#e=3
+#for d in range(1,100):
+#        if ((e*d % PHI)==1): break
+#print (e,N)
+#print (d,N)
+#M=4
+#cipher = M**e % N
+#print (cipher)
+#message = cipher**d % N
+#print (message)
 
